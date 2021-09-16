c232c82d8a44fd50dfbe0a52d844604a2f2987c1
shyamtarams <shyamkrishnanpv535@gmail.com>
2021-09-16 19:20:18 +0530

    db config changes

--- shoppie/settings.py
+++ shoppie/settings.py
@@ -95,7 +95,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'shoppie_db',
         'USER':'postgres', 
-        'PASSWORD': '*******',
+        'PASSWORD': '********',
         'HOST': '127.0.0.1', 
         'PORT': '5432',
      }
