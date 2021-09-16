42644154d551562d64aa064c03e79eb9120f4dcf
shyamtarams <shyamkrishnanpv535@gmail.com>
2021-09-16 19:13:42 +0530

    rest api on home app

--- shoppie/settings.py
+++ shoppie/settings.py
@@ -91,7 +91,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'shoppie_db',
         'USER':'postgres', 
-        'PASSWORD': 'pg@shyam535',
+        'PASSWORD': '*******',
         'HOST': '127.0.0.1', 
         'PORT': '5432',
      }
